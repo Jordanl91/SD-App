d3908bcf2887c18c727f3ed4ab47c4121bea0f0c
Phanidhar Mothukuri <phanidharmothukuri@Phanidhars-Mac-mini.local>
2017-08-24 19:34:05 -0400

    Initial Commit

---
+++ Smart Drinks & Nutrition/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Smart Drinks & Nutrition
+//
+//  Created by Phanidhar Mothukuri on 8/24/17.
+//  Copyright © 2017 TechSoft,Inc. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
