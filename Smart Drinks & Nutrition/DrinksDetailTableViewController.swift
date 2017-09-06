1a4f5a426c1fb81782d308b06c8e1d7badebb732
Phanidhar Mothukuri <phani@techsoftinc.net>
2017-09-06 01:26:37 -0400

    Drinks detail view.

---
+++ Smart Drinks & Nutrition/DrinksDetailTableViewController.swift
@@ -0,0 +1,122 @@
+//
+//  DrinksDetailTableViewController.swift
+//  Smart Drinks & Nutrition
+//
+//  Created by Phanidhar Mothukuri on 9/6/17.
+//  Copyright © 2017 TechSoft,Inc. All rights reserved.
+//
+
+import UIKit
+
+class DrinksDetailTableViewController: UITableViewController {
+    var drinksArrayOfDictionary = [[String:Any]]()
+    var drinkNames = [String]()
+    var drinkRecipe = [String]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.navigationController?.navigationBar.barTintColor = UIColor.init(red: 255/255, green: 70/255, blue: 50/255, alpha: 1)
+        tableView.backgroundColor = UIColor.init(red: 255/255, green: 70/255, blue: 50/255, alpha: 1)
+        //self.navigationController?.navigationBar.tintColor = UIColor.white
+        self.navigationController!.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName: UIColor.white]
+        self.navigationController?.navigationBar.isTranslucent = false
+        tableView.tableFooterView = UIView()
+        
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        for drinkDescription in drinksArrayOfDictionary{
+            drinkNames.append(drinkDescription["name"] as! String)
+            drinkRecipe.append(drinkDescription["description"] as! String)
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        if drinkRecipe.count != 0{
+            return drinksArrayOfDictionary.count
+        }else{
+            return drinkNames.count
+        }
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "DETAIL_DRINKS_CELL")
+        cell?.textLabel?.text = drinkNames[indexPath.row]
+        cell?.textLabel?.textColor = UIColor.white
+        cell?.detailTextLabel?.textColor = UIColor.white
+        if drinkRecipe.count != 0{
+            cell?.detailTextLabel?.text = drinkRecipe[indexPath.row]
+        }else{
+            cell?.detailTextLabel?.text = ""
+        }
+        return cell!
+    }
+
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 60
+    }
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Smart Drinks & Nutrition/FirstViewController.swift
+++ Smart Drinks & Nutrition/FirstViewController.swift
@@ -9,9 +9,23 @@
 import UIKit
 
 class FirstViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-
+    let drinksCategories = ["Smart smoothies","Fruit smoothies", "Boosters", "Additional Flavors"]
+    var datasourceDictionary = [String:Any]()
+    var drinksArrayOfDictionary = [[String:Any]]()
+    var drinksArray = [String]()
+    @IBOutlet weak var drinksTableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.navigationController?.navigationBar.barTintColor = UIColor.init(red: 255/255, green: 70/255, blue: 50/255, alpha: 1)
+        drinksTableView.backgroundColor = UIColor.init(red: 255/255, green: 70/255, blue: 50/255, alpha: 1)
+        //self.navigationController?.navigationBar.tintColor = UIColor.white
+        self.navigationController!.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName: UIColor.white]
+        self.navigationController?.navigationBar.isTranslucent = false
+        drinksTableView.tableFooterView = UIView()
+        let pathStr = Bundle.main.path(forResource: "Drinks", ofType: "plist")
+        let data :NSData? = NSData(contentsOfFile: pathStr!)
+        datasourceDictionary = try! PropertyListSerialization.propertyList(from: data! as Data, options: [], format: nil) as! [String:Any]
+        print(datasourceDictionary.self)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -22,14 +36,44 @@ class FirstViewController: UIViewController, UITableViewDelegate, UITableViewDat
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "DRINKS_CELL")
-        cell?.textLabel?.text = "Drinks \(indexPath.row)"
+        cell?.textLabel?.text = drinksCategories[indexPath.row]
+        cell?.textLabel?.textColor = UIColor.white
         return cell!
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return drinksCategories.count
     }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        // get the detals screen data and perform segue.
+        if indexPath.row < 2{
+            drinksArrayOfDictionary = datasourceDictionary[drinksCategories[indexPath.row]] as! [[String : Any]]
+        }else{
+            drinksArray = datasourceDictionary[drinksCategories[indexPath.row]] as! [String]
+        }
+        performSegue(withIdentifier: "Drinks_Detail_Segue", sender: indexPath.row)
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 60
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let destination = segue.destination as? DrinksDetailTableViewController{
+            if let index = sender as? Int{
+                if index < 2{
+                    destination.drinksArrayOfDictionary = self.drinksArrayOfDictionary
+                    destination.title = drinksCategories[index]
+                }else{
+                    destination.drinkNames = self.drinksArray
+                    destination.title = drinksCategories[index]
+                }
+            }
 
-
+        }
+        
+    }
+    
 }
 
