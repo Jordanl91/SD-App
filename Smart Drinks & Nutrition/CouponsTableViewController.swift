c87d3f4517339abb1ccf413f1b609969a6834aa6
Phanidhar Mothukuri <phani@techsoftinc.net>
2017-10-09 12:53:15 -0400

    Coupons skeleton.

---
+++ Smart Drinks & Nutrition/CouponsTableViewController.swift
@@ -0,0 +1,98 @@
+//
+//  CouponsTableViewController.swift
+//  Smart Drinks & Nutrition
+//
+//  Created by Phanidhar Mothukuri on 10/9/17.
+//  Copyright © 2017 TechSoft,Inc. All rights reserved.
+//
+
+import UIKit
+
+class CouponsTableViewController: UITableViewController {
+    var couponsDictionary = [[String:Any]]()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let pathStr = Bundle.main.path(forResource: "Coupons", ofType: "plist")
+        let data :NSData? = NSData(contentsOfFile: pathStr!)
+        couponsDictionary = try! PropertyListSerialization.propertyList(from: data! as Data, options: [], format: nil) as! [[String:Any]]
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return couponsDictionary.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "COUPON_CELL", for: indexPath)
+
+        // Configure the cell...
+        cell.textLabel?.text = couponsDictionary[indexPath.row]["title"] as? String
+        cell.detailTextLabel?.text = couponsDictionary[indexPath.row]["subtitle"] as? String
+        return cell
+    }
+
+    
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
